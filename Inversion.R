3b089cb79673b7f2bd819c3c8580ff1f269edee7
Botond Sipos <sbotond@ebi.ac.uk>
2012-10-28 12:26:06 +0000

    Documentation update.

--- Inversion.R
+++ Inversion.R
@@ -1,20 +1,20 @@
 #  Copyright (C) 2012 by Botond Sipos, European Bioinformatics Institute
 #  sbotond@ebi.ac.uk
 #
-#  This file is part of the pcrcoal software for coalescent simulations of PCR reactions.
+#  This file is part of the simcopy software for coalescent simulations of PCR reactions.
 #
-#  pcrcoal is free software: you can redistribute it and/or modify
+#  simcopy is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  pcrcoal is distributed in the hope that it will be useful,
+#  simcopy is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with pcrcoal. If not, see <http://www.gnu.org/licenses/>.
+#  along with simcopy. If not, see <http://www.gnu.org/licenses/>.
 
 # Construct an invertor process sampling lengths from a geometric+1 distribution:
 .construct.invertor<-function(rate, lenMean, lenMax){
