cc6489e43ee109b20af4127bfe99ec6efa577954
Botond Sipos <sbotond@ebi.ac.uk>
2013-04-05 15:00:50 +0100

    License update.

--- Duplication.R
+++ Duplication.R
@@ -1,20 +1,29 @@
-#  Copyright (C) 2012 by Botond Sipos, European Bioinformatics Institute
-#  sbotond@ebi.ac.uk
 #
-#  This file is part of the simcopy software for coalescent simulations of PCR reactions.
+# Copyright (C) 2013 EMBL - European Bioinformatics Institute
 #
-#  simcopy is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
+# This program is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-#  simcopy is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with simcopy. If not, see <http://www.gnu.org/licenses/>.
+# Neither the institution name nor the name simcopy
+# can be used to endorse or promote products derived from
+# this software without prior written permission. For
+# written permission, please contact <sbotond@ebi.ac.uk>.
+
+# Products derived from this software may not be called
+# simcopy nor may simcopy appear in their
+# names without prior written permission of the developers.
+# You should have received a copy of the GNU General Public
+# License along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
 
 # Construct a duplicator process sampling lengths from a geometric+1 distribution:
 .construct.duplicator<-function(rate, lenMean, lenMax){
